6564b8b8cb75aa55dd964bd543bdc3a1a005f614
Garrett D'Amore <garrett@damore.org>
2021-10-27 22:34:41 -0700

    fixes #1346 windows ipc winsec fails frequently in CI/CD

---
+++ src/platform/windows/win_ipc_sec_test.c
@@ -0,0 +1,197 @@
+//
+// Copyright 2021 Staysail Systems, Inc. <info@staysail.tech>
+// Copyright 2018 Capitar IT Group BV <info@capitar.com>
+//
+// This software is supplied under the terms of the MIT License, a
+// copy of which should be located in the distribution where this
+// file was obtained (LICENSE.txt).  A copy of the license may also be
+// found online at https://opensource.org/licenses/MIT.
+//
+
+#include <nng/nng.h>
+#include <nng/protocol/reqrep0/rep.h>
+#include <nng/protocol/reqrep0/req.h>
+#include <nng/transport/ipc/ipc.h>
+#include <nuts.h>
+
+#include "convey.h"
+#include "stubs.h"
+#include "trantest.h"
+
+#define ADDR "/tmp/ipc_winsec_test"
+
+// Inproc tests.
+
+#include <assert.h>
+
+// Microsoft prefers CamelCase header names, but relies on case insensitive
+// file systems to make that work.  The rest of the world (min-gw64 included)
+// uses case-sensitive names and lowercase.
+
+#include <accctrl.h>
+
+#include <sddl.h>
+
+#include <aclapi.h>
+
+SECURITY_DESCRIPTOR *
+sdescAuthUsers(PSID sid, PACL *aclp)
+{
+	SECURITY_DESCRIPTOR *sdesc;
+	EXPLICIT_ACCESS      xa;
+	ACL                 *acl;
+
+	sdesc = calloc(SECURITY_DESCRIPTOR_MIN_LENGTH, 1);
+	assert(sdesc != NULL);
+
+	InitializeSecurityDescriptor(sdesc, SECURITY_DESCRIPTOR_REVISION);
+
+	xa.grfAccessPermissions = GENERIC_READ | GENERIC_WRITE;
+	xa.grfAccessMode        = SET_ACCESS;
+	xa.grfInheritance       = SUB_CONTAINERS_AND_OBJECTS_INHERIT;
+	xa.Trustee.TrusteeForm  = TRUSTEE_IS_SID;
+	xa.Trustee.TrusteeType  = TRUSTEE_IS_WELL_KNOWN_GROUP;
+	xa.Trustee.ptstrName    = (LPSTR) sid;
+
+	SetEntriesInAcl(1, &xa, NULL, &acl);
+	*aclp = acl;
+
+	SetSecurityDescriptorDacl(sdesc, TRUE, acl, FALSE);
+	return (sdesc);
+}
+
+void
+test_ipc_security_descriptor(void)
+{
+	nng_stream_listener *l;
+	char                 address[64];
+	char                 pipe[64];
+	SECURITY_DESCRIPTOR *sd;
+	SID                  users;
+	DWORD                size;
+	PACL                 acl = NULL;
+	PACL                 dacl;
+	PSECURITY_DESCRIPTOR psd;
+	PACE_HEADER          ace;
+	PSID                 psid;
+	PACCESS_ALLOWED_ACE  allowed;
+
+	nuts_scratch_addr("ipc", sizeof(address), address);
+
+	NUTS_PASS(nng_stream_listener_alloc(&l, address));
+	size = sizeof(users);
+	CreateWellKnownSid(WinAuthenticatedUserSid, NULL, &users, &size);
+	sd = sdescAuthUsers(&users, &acl);
+
+	NUTS_ASSERT(sd != NULL);
+	NUTS_ASSERT(acl != NULL);
+
+	NUTS_PASS(nng_listener_setopt_ptr(
+	    l, NNG_OPT_IPC_SECURITY_DESCRIPTOR, sdesc));
+	NUTS_PASS(nng_listener_start(l, 0));
+
+	snprintf(pipe, sizeof(pipe, "\\\\.\\\\pipe\\%s", address)) HANDLE ph =
+	    CreateFileA(pipe, READ_CONTROL, 0, NULL, OPEN_EXISTING,
+	        FILE_FLAG_OVERLAPPED, NULL);
+
+	NUTS_ASSERT(ph != INVALID_HANDLE_VALUE);
+
+	NUTS_ASSERT(
+	    GetSecurityInfo(ph, SE_KERNEL_OBJECT, DACL_SECURITY_INFORMATION,
+	        NULL, NULL, &dacl, NULL, &psd) == ERROR_SUCCESS);
+
+	NUTS_ASSERT(dacl->AceCount == 1);
+	NUTS_ASSERT(GetAce(dacl, 0, (void **) &ace) == TRUE);
+	allowed = (PACCESS_ALLOWED_ACE) ace;
+	psid    = (PSID) &allowed->SidStart;
+	NUTS_ASSERT(IsValidSid(psid));
+	NUTS_ASSERT(EqualSid(asid, &users) == TRUE);
+
+	CloseHandle(ph);
+	free(sd);
+	LocalFree(acl);
+	LocalFree(psd);
+	nng_stream_listener_close(l);
+	nng_stream_listener_free(l);
+}
+
+void
+test_ipc_security_descriptor_busy(void)
+{
+	// This test ensures that the descriptor can only be set before
+	// the listener is started.
+	nng_stream_listener *l;
+	char                 address[64];
+	SECURITY_DESCRIPTOR *sd;
+	SID                  users;
+	DWORD                size;
+	PACL                 acl = NULL;
+
+	nuts_scratch_addr("ipc", sizeof(address), address);
+
+	NUTS_PASS(nng_stream_listener_alloc(&l, address));
+	size = sizeof(users);
+	CreateWellKnownSid(WinAuthenticatedUserSid, NULL, &users, &size);
+	sd = sdescAuthUsers(&users, &acl);
+
+	NUTS_ASSERT(sd != NULL);
+	NUTS_ASSERT(acl != NULL);
+
+	NUTS_PASS(nng_listener_start(l, 0));
+
+	NUTS_FAIL(
+	    nng_listener_setopt_ptr(l, NNG_OPT_IPC_SECURITY_DESCRIPTOR, sdesc),
+	    NNG_EBUSY);
+
+	free(sd);
+	nng_stream_listener_close(l);
+	nng_stream_listener_free(l);
+}
+
+void
+test_ipc_security_descriptor_bogus(void)
+{
+	nng_stream_listener *l;
+	char                 address[64];
+
+	nuts_scratch_addr("ipc", sizeof(address), address);
+
+	NUTS_PASS(nng_stream_listener_alloc(&l, address));
+
+	NUTS_FAIL(
+	    nng_listener_setopt_ptr(l, NNG_OPT_IPC_SECURITY_DESCRIPTOR, NULL),
+	    NNG_EINVAL);
+
+	nng_stream_listener_close(l);
+	nng_stream_listener_free(l);
+}
+
+void
+test_ipc_security_descriptor_dialer(void)
+{
+	nng_stream_dialer   *d;
+	char                 address[64];
+	SECURITY_DESCRIPTOR *sdesc;
+
+	nuts_scratch_addr("ipc", sizeof(address), address);
+	NUTS_PASS(nng_Stream_dialer_alloc(&d, address));
+
+	sdesc = calloc(SECURITY_DESCRIPTOR_MIN_LENGTH, 1);
+	NUTS_ASSERT(sdesc != NULL);
+	InitializeSecurityDescriptor(sdesc, SECURITY_DESCRIPTOR_REVISION);
+	NUTS_FAIL(
+	    nng_dialer_setopt_ptr(d, NNG_OPT_IPC_SECURITY_DESCRIPTOR, sdesc),
+	    NNG_ENOTSUP);
+	free(sdesc);
+	nng_stream_dialer_free(d);
+}
+
+NUTS_TESTS = {
+	{ "ipc security descriptor", test_ipc_security_descriptor },
+	{ "ipc security descriptor busy", test_ipc_security_descriptor_busy },
+	{ "ipc security descriptor bogus",
+	    test_ipc_security_descriptor_bogus },
+	{ "ipc security descriptor dialer",
+	    test_ipc_security_descriptor_dialer },
+	{ NULL, NULL },
+};
